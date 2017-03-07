ce4f485242a7aaa7101c876a18099faef6995009
ishaypol <ishaypol@gmail.com>
2017-03-07 15:30:29 +0200

    component structure completed meta data of images is being downloaded succefully from flickr

--- src/App.js
+++ src/App.js
@@ -1,43 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+// import Flickr from "flickrapi"
 
 
-var Photo = React.createClass({
-    render: function() {
+function Photo(props){
         return (
-            <img src="C:\Users\Ishay\Pictures\Feedback\{B8EF1783-573B-4A4E-A6BD-48AEF1A4E7D1}\Capture001.png"/>
+            <img src={props.source} alt={props.source} />
+        );
+}
+
+var PhotoPan = React.createClass({
+
+    render: function () {
+        return (
+            <ul>
+                {
+                    this.props.photos.map((source)=> {
+                        return(
+                            <li key={source}>
+                                <Photo source={source} />
+                            </li>
+                        );
+                    })
+                }
+            </ul>
         );
     }
 });
 
-var photoPane = React.createClass({
-    photos: [],
+var Layout = React.createClass({
+    getInitialState: function () {
+        return {photos: []};
+    },
+    searchPhotos: function (query) {
+        var api_key = 'b4f935ecf2ae1382defc3b002a7f6b80';
+        var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&' +
+            'api_key=' + api_key + '&text=' + query + '&format=json&per_page=5&nojsoncallback=1';
+
+        axios.get(url).then(res => {
+                var res_dict = res.data.photos.photo;
+                this.setState({ photos: res_dict });
+            });
+    },
     render: function () {
-        return <h1> asdsadsa</h1>;
+        return (
+            <div>
+                <h1> My Photo Gallery</h1>
+                <SearchField searchPhotos={this.searchPhotos}/>
+                <PhotoPan photos={this.state.photos}/>
+            </div>
+        );
     }
 });
 
 var SearchField = React.createClass({
 
-    search: function () {
-        alert('asdasdsa');
+    getInitialState: function () {
+        return {value: ""}
+    },
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    },
+
+    handleSubmit: function (event) {
+        event.preventDefault();
+        this.props.searchPhotos(this.state.value);
     },
    render: function () {
        return(
-           <form>
-               <input type="text"  default="Search photos here" onee={this.search} />
+           <form onSubmit={this.handleSubmit} >
+               <input type="text"  value={this.state.value}  onChange={this.handleChange}/>
+               <input type="submit" value="submit"/>
            </form>
        );
    }
 });
 
+
 class App extends Component {
   render() {
     return (
-      <div>
-          <SearchField />
-          <Photo> hello</Photo>
-      </div>
+          <Layout/>
     );
   }
 }
